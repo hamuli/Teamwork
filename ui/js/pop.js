62d257a23e1579a8f21f1068499a11f5fa115aee
glodie <glodiehamul@gmail.com>
2019-09-10 21:03:25 +0300

    feat(user-profil): this commit deliver a user profil where a user can delete , edit , comment and post .[fimishes -168412767]

---
+++ ui/js/pop.js
@@ -0,0 +1,10 @@
+
+document.querySelector('#button').addEventListener('click',function(){
+
+document.querySelector('.popup').style.display ='flex';
+console.log(element);
+});
+document.querySelector('.i').addEventListener('click', function(){
+  document.querySelector('.popup').style.display='none';
+});
+
