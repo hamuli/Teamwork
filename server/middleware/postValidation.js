4910b81b580a8ad9edfe1f28ade056b9f44c4d46
glodie <glodiehamul@gmail.com>
2019-09-29 02:14:36 +0300

    feat(delete-post): implement update end point [finishes  #168822477]

---
+++ server/middleware/postValidation.js
@@ -0,0 +1,14 @@
+import { check } from 'express-validator';
+
+export default [
+  check('title')
+    .exists({ checkFalsy: true })
+    .withMessage(' article title  is required')
+    .isLength({ min: 2 })
+    .withMessage('must be at least 2 characters long'),
+  check('article')
+    .exists({ checkFalsy: true })
+    .withMessage('article post is required')
+    .isLength({ min: 2 })
+    .withMessage('must be at least 2 characters long')
+];
