8c85de51895ac72513bc7dba1090ba9c9e46e9a2
glodie <glodiehamul@gmail.com>
2019-10-01 11:42:14 +0300

    bg(fixe-error):make user interface responsive [finishes #168750194]

--- ui/js/pop.js
+++ ui/js/pop.js
@@ -1,56 +1,63 @@
+/* eslint-disable semi */
 const builder = document.querySelectorAll('.build');
-if(builder){
-	builder.forEach((item) => {
-		item.addEventListener('click', e => {
-			let target = e.target.parentNode.children[1];
-			if(!target.classList.contains('show')){
-				target.classList.add('show');
-				target.classList.remove('hide');
-			}
-			else{
-				target.classList.remove('show');
-				target.classList.add('hide');
-			}
-		})
-	})
+
+if (builder) {
+  builder.forEach((item)=> {
+    item.addEventListener('click', (e)=> {
+      let target = e.target.parentNode.children[1];
+
+      if (!target.classList.contains('show')) {
+        target.classList.add('show');
+        target.classList.remove('hide');
+      } else {
+        target.classList.remove('show');
+        target.classList.add('hide');
+      }
+    })
+  })
 }
 
 let myEdit = document.querySelectorAll('.mybutton');
-for (var i = 0;i<myEdit.length;  i++) {
- myEdit[i].addEventListener('click', function(){
- 	if(document.querySelector('.pg-modal')) return document.querySelector('.pg-modal').style.display="none";
- })
+
+// eslint-disable-next-line no-var
+// eslint-disable-next-line vars-on-top
+for (var i = 0; i < myEdit.length; i++) {
+  // eslint-disable-next-line no-loop-func
+  myEdit[i].addEventListener('click', ()=> {
+ 	if (document.querySelector('.pg-modal')) return document.querySelector('.pg-modal').style.display = 'none';
+  })
 }
 
-/*document.querySelector('.mybutton').addEventListener('click', function(){
+/* document.querySelector('.mybutton').addEventListener('click', function(){
 
 	document.querySelector('.modal-contents').style.display='flex';
-});*/
+}); */
 let pagedeleted = document.querySelector('.pg-delete');
-let model=document.querySelectorAll('.itemDeleted');
+let model = document.querySelectorAll('.itemDeleted');
 
-for(let i=0; i < model.length; i++){
-  model[i].onclick=function() {
-  	pagedeleted.style.display = "flex";
+for (let i = 0; i < model.length; i++) {
+  model[i].onclick = function () {
+  	pagedeleted.style.display = 'flex';
   	console.log(pagedeleted);
   }
 }
 
 let mybutton = document.querySelectorAll('.cancelPost, .deletePost');
-for (var i = 0;i<mybutton.length;  i++) {
- mybutton[i].addEventListener('click', function(){
- 	document.querySelector('.pg-delete').style.display="none"
- })
+
+for (var i = 0; i < mybutton.length; i++) {
+  mybutton[i].addEventListener('click', ()=> {
+ 	document.querySelector('.pg-delete').style.display = 'none'
+  })
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == pg_modal) {
-    pg_modal.style.display = "none";
+    pg_modal.style.display = 'none';
   }
   if (event.target == pagedeleted) {
-    pagedeleted.style.display = "none";
+    pagedeleted.style.display = 'none';
   }
-  if(!event.target.matches('.popup')){
+  if (!event.target.matches('.popup')) {
   	console.log(event.target);
   }
 }
